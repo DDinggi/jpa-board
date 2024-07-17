911783a158ee7e97b52d5b0b2c3fb3616caf6aa1
DDingGI <201911012@sangmyung.kr>
2024-07-17 16:48:14 +0900

    Initial commit

--- src/main/java/com/study/domain/comment/CommentApiController.java
+++ src/main/java/com/study/domain/comment/CommentApiController.java
@@ -1,9 +1,14 @@
 package com.study.domain.comment;
 
+import com.study.common.paging.PagingResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +23,13 @@ public class CommentApiController {
         return commentService.findCommentById(id);
     }
 
+
     // 댓글 리스트 조회
     @GetMapping("/posts/{postId}/comments")
-    public List<CommentResponse> findAllComment(@PathVariable final Long postId) {
-        return commentService.findAllComment(postId);
+    public PagingResponse<CommentResponse> findAllComment(@PathVariable final Long postId, final CommentSearchDto params) {
+        return commentService.findAllComment(params);
     }
+
 
     // 댓글 상세정보 조회
     @GetMapping("/posts/{postId}/comments/{id}")
@@ -37,6 +44,7 @@ public class CommentApiController {
         commentService.updateComment(params);
         return commentService.findCommentById(id);
     }
+
 
     // 댓글 삭제
     @DeleteMapping("/posts/{postId}/comments/{id}")
