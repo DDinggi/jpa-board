ecc3ed9f4ad44596c2a59187d0bd27cc3d97d04e
DDingGI <201911012@sangmyung.kr>
2024-09-01 18:05:05 +0900

    게시판 분류 추가

--- src/main/java/com/study/domain/post/PostService.java
+++ src/main/java/com/study/domain/post/PostService.java
@@ -3,18 +3,21 @@ package com.study.domain.post;
 import com.study.common.dto.SearchDto;
 import com.study.common.paging.Pagination;
 import com.study.common.paging.PagingResponse;
+import com.study.domain.file.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;  // 추가된 임포트
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostMapper postMapper;
+    private final FileService fileService; // FileService 추가
 
     /**
      * 게시글 저장
@@ -52,7 +55,12 @@ public class PostService {
      * @param id - PK
      * @return PK
      */
+    @Transactional
     public Long deletePost(final Long id) {
+        // 게시글에 연관된 파일 삭제
+        fileService.deleteAllFileByIds(Arrays.asList(id));  // List<Long>로 변환
+
+        // 게시글 삭제
         postMapper.deleteById(id);
         return id;
     }
@@ -63,20 +71,15 @@ public class PostService {
      * @return list & pagination information
      */
     public PagingResponse<PostResponse> findAllPost(final SearchDto params) {
-
-        // 조건에 해당하는 데이터가 없는 경우, 응답 데이터에 비어있는 리스트와 null을 담아 반환
         int count = postMapper.count(params);
         if (count < 1) {
             return new PagingResponse<>(Collections.emptyList(), null);
         }
 
-        // Pagination 객체를 생성해서 페이지 정보 계산 후 SearchDto 타입의 객체인 params에 계산된 페이지 정보 저장
         Pagination pagination = new Pagination(count, params);
         params.setPagination(pagination);
 
-        // 계산된 페이지 정보의 일부(limitStart, recordSize)를 기준으로 리스트 데이터 조회 후 응답 데이터 반환
         List<PostResponse> list = postMapper.findAll(params);
         return new PagingResponse<>(list, pagination);
     }
-
 }
